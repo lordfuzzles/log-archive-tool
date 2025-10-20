729c3f09b74ececf3389d719739393d9de3ead58
lordfuzzles <lord.fuzzles@gmail.com>
2025-10-19 21:38:08 -0500

    Re-did all quotations to avoid continuing errors. Script works successfully now.

--- log-archive.sh
+++ log-archive.sh
@@ -3,7 +3,7 @@
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 SOURCE_DIR=$1
 ARCHIVE_DIR="$HOME/Archive/logs"
-ARCHIVE_NAME="logs_archive_$TIMESTAMP.tar.gz
+ARCHIVE_NAME="logs_archive_$TIMESTAMP.tar.gz"
 
 if [ "$#" -eq 0 ]; then
     echo "ERROR: No Source Provided. Archive Not Created."
@@ -12,22 +12,22 @@ fi
 
 # Verify if the archive and logs directories exist and creates them if they don't
 verify_archive_dir() {
-    if [ -d "$ARCHIVE_DIR"]; then
+    if [ -e "$ARCHIVE_DIR" ]; then
         echo "Archive Directory Found."
     else
         echo "Archive Directory Not Found. Creating Archive Directory..."
-        mkdir -p $ARCHIVE_DIR
+        mkdir -p "$ARCHIVE_DIR"
         verify_archive_dir
     fi
 }
 
 # Creates the log archive file and verifies that it has been created
 create_archive() {
-    if [ -f "$ARCHIVE_DIR/$ARCHIVE_NAME" ]; then
+    if [ -e "$ARCHIVE_DIR/$ARCHIVE_NAME" ]; then
         echo "$ARCHIVE_NAME Created Successfully."
     else
         echo "Creating $ARCHIVE_NAME..."
-        tar -czvf "$ARCHIVE_DIR/$ARCHIVE_NAME" $SOURCE_DIR
+        tar -czvf "$ARCHIVE_DIR/$ARCHIVE_NAME" "$SOURCE_DIR"
         create_archive
     fi
 }
